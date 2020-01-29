59aef9c9c1adf363e0f13261c2bf5d85cb860ea4
Your Name <you@example.com>
2020-01-29 12:00:44 -0800

    Update calculator to Bri and Hana's version

--- calculator.py
+++ calculator.py
@@ -1,76 +1,50 @@
-"""A prefix-notation calculator."""
-
+"""A prefix-notation calculator.
+    
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
 
 from arithmetic import *
 
 
+# Your code goes here
 while True:
-    user_input = input("> ")
-    tokens = user_input.split(" ")
 
-    if "q" in tokens:
-        print("You will exit.")
+    input_string = input(">")
+    tokens = input_string.split(" ")
+
+    num1 = float(tokens[1])
+    
+    if len(tokens) > 2:
+        num2 = float(tokens[2])
+
+    operand = tokens[0]
+
+    if operand == "q":
         break
 
-    elif len(tokens) < 2:
-        print("Not enough inputs.")
-        continue
-
-    operator = tokens[0]
-    num1 = tokens[1]
-
-    if len(tokens) < 3:
-        num2 = "0"
-
     else:
-        num2 = tokens[2]
 
-    if len(tokens) > 3:
-        num3 = tokens[3]
+        if operand == "+":
+            print(add(num1, num2))
 
-    # A place to store the return value of the math function we call,
-    # to give us one clear place where that result is printed.
-    result = None
+        elif operand == "-":
+            print(subtract(num1, num2))
 
-    if not num1.isdigit() or not num2.isdigit():
-        print("Those aren't numbers!")
-        continue
+        elif operand == "*":
+            print(multiply(num1, num2))
 
-    # We have to cast each value we pass to an arithmetic function from a
-    # a string into a numeric type. If we use float across the board, all
-    # results will have decimal points, so let's do that for consistency.
+        elif operand == "/":
+            print(divide(num1, num2))
 
-    elif operator == "+":
-        result = add(float(num1), float(num2))
+        elif operand == "square":
+            print(square(num1))
 
-    elif operator == "-":
-        result = subtract(float(num1), float(num2))
+        elif operand == "cube":
+            print(cube(num1))
 
-    elif operator == "*":
-        result = multiply(float(num1), float(num2))
+        elif operand == "pow":
+            print(power(num1, num2))
 
-    elif operator == "/":
-        result = divide(float(num1), float(num2))
-
-    elif operator == "square":
-        result = square(float(num1))
-
-    elif operator == "cube":
-        result = cube(float(num1))
-
-    elif operator == "pow":
-        result = power(float(num1), float(num2))
-
-    elif operator == "mod":
-        result = mod(float(num1), float(num2))
-
-    elif operator == "x+":
-        result = add_mult(float(num1), float(num2), float(num3))
-
-    elif operator == "cubes+":
-        result = add_cubes(float(num1), float(num2))
-
-    else:
-        result = "Please enter an operator followed by two integers."
-
-    print(result)
+        elif operand == "mod":
+            print(mod(num1, num2))
